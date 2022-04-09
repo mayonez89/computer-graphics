37020d448140c699a9562f14f3f971333c0163d1
= <=>
2022-04-09 11:30:41 +0300

    assignment 2 added

--- Assignment1/CG_assignment1.py
+++ Assignment1/CG_assignment1.py
@@ -6,15 +6,16 @@ from utils.mvp_controller import MVPController
 from OpenGL.GL import shaders
 import random
 import time
+import numpy as np
 
 # Define a tetrahedron using triangles
 vertex_buffer_data = [
-	-1, +0, -1, +1, +0, -1, -1, +0, +1,  # Base 0
-	+1, +0, -1, +1, +0, +1, -1, +0, +1,  # Base 1
-	-1, +0, -1, +0, +1, +0, +1, +0, -1,  # Side 0
-	+1, +0, -1, +0, +1, +0, +1, +0, +1,  # Side 1
-	+1, +0, +1, +0, +1, +0, -1, +0, +1,  # Side 2
-	-1, +0, +1, +0, +1, +0, -1, +0, -1,  # Side 3
+    -1, +0, -1, +1, +0, -1, -1, +0, +1,  # Base 0
+    +1, +0, -1, +1, +0, +1, -1, +0, +1,  # Base 1
+    -1, +0, -1, +0, +1, +0, +1, +0, -1,  # Side 0
+    +1, +0, -1, +0, +1, +0, +1, +0, +1,  # Side 1
+    +1, +0, +1, +0, +1, +0, -1, +0, +1,  # Side 2
+    -1, +0, +1, +0, +1, +0, -1, +0, -1,  # Side 3
 ]
 # Set random colors for each of the vertices
 color_buffer_data = [random.random() for _ in range(len(vertex_buffer_data))]
@@ -22,98 +23,98 @@ color_buffer_data = [random.random() for _ in range(len(vertex_buffer_data))]
 start_time = time.time()
 
 
-def read_file(file_path: str) -> str:
-	"""Reads a text file given a path and returns it as a string."""
-	with open(file_path, mode="r") as f:
-		contents = f.readlines()
-	return contents
+def read_file(file_path: str) -> list:
+    """Reads a text file given a path and returns it as a string."""
+    with open(file_path, mode="r") as f:
+        contents = f.readlines()
+    return contents
 
 
 class GLContext:
-	"""Used for storing context data in the main window."""
-	pass
+    """Used for storing context data in the main window."""
+    pass
 
 
 class Win(GlutWindow):
-	"""The main application. Inherits from glut_window.py."""
-	def __init__(self, width: int = 800, height: int = 480):
-		super().__init__(width, height)
-		self.context = GLContext()
+    """The main application. Inherits from glut_window.py."""
 
-	def init_context(self):
-		# Read shader files and compile them
-		vertex_shader_string = read_file("shaders/vertex_shader.glsl")
-		fragment_shader_string = read_file("shaders/fragment_shader.glsl")
-		vertex_shader = shaders.compileShader(vertex_shader_string, GL_VERTEX_SHADER)
-		fragment_shader = shaders.compileShader(
-			fragment_shader_string, GL_FRAGMENT_SHADER
-		)
-		self.shader_program = shaders.compileProgram(vertex_shader, fragment_shader)
+    def __init__(self, width: int = 800, height: int = 480):
+        super().__init__(width, height)
+        self.context = GLContext()
 
-		# Get location of the MVP matrix
-		self.context.mvp_location = glGetUniformLocation(self.shader_program, "mvp")
-		# Generate buffers for vertices and color data and buffer the data
-		self.context.vertex_buffer = glGenBuffers(1)
-		glBindBuffer(GL_ARRAY_BUFFER, self.context.vertex_buffer)
-		glBufferData(
-			GL_ARRAY_BUFFER,
-			len(vertex_buffer_data) * 4,
-			(GLfloat * len(vertex_buffer_data))(*vertex_buffer_data),
-			GL_STATIC_DRAW,
-		)
+    def init_context(self):
+        # Read shader files and compile them
+        vertex_shader_string = read_file("shaders/vertex_shader.glsl")
+        fragment_shader_string = read_file("shaders/fragment_shader.glsl")
+        vertex_shader = shaders.compileShader(vertex_shader_string, GL_VERTEX_SHADER)
+        fragment_shader = shaders.compileShader(
+            fragment_shader_string, GL_FRAGMENT_SHADER
+        )
+        self.shader_program = shaders.compileProgram(vertex_shader, fragment_shader)
 
-		self.context.color_buffer = glGenBuffers(1)
-		glBindBuffer(GL_ARRAY_BUFFER, self.context.color_buffer)
-		glBufferData(
-			GL_ARRAY_BUFFER,
-			len(color_buffer_data) * 4,
-			(GLfloat * len(color_buffer_data))(*color_buffer_data),
-			GL_STATIC_DRAW,
-		)
+        # Get location of the MVP matrix
+        self.context.mvp_location = glGetUniformLocation(self.shader_program, "mvp")
+        # Generate buffers for vertices and color data and buffer the data
+        self.context.vertex_buffer = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.context.vertex_buffer)
+        glBufferData(
+            GL_ARRAY_BUFFER,
+            len(vertex_buffer_data) * 4,
+            (GLfloat * len(vertex_buffer_data))(*vertex_buffer_data),
+            GL_STATIC_DRAW,
+        )
 
-	def calc_mvp(self):
-		self.calc_model()
-		self.context.mvp = self.controller.calc_mvp(self.model_matrix)
+        self.context.color_buffer = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.context.color_buffer)
+        glBufferData(
+            GL_ARRAY_BUFFER,
+            len(color_buffer_data) * 4,
+            (GLfloat * len(color_buffer_data))(*color_buffer_data),
+            GL_STATIC_DRAW,
+        )
 
-	def resize(self, width, height):
-		glViewport(0, 0, width, height)
-		self.calc_mvp()
+    def calc_mvp(self):
+        self.calc_model()
+        self.context.mvp = self.controller.calc_mvp(self.model_matrix)
 
-	def calc_model(self):
-		self.model_matrix = glm.mat4(1)
-		# 2. Add code here to make the object rotate
+    def resize(self, width, height):
+        glViewport(0, 0, width, height)
+        self.calc_mvp()
 
+    def calc_model(self, speed=2):
+        # self.model_matrix = glm.mat4(1)
+        # 2. Add code here to make the object rotate
+        self.model_matrix = glm.rotate(glm.mat4(1), time.time() * speed, glm.vec3(1, 0, 0))
 
+    def draw(self):
+        """
+        The main drawing function. Is called whenever an update occurs.
+        """
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.calc_mvp()
+        glUseProgram(self.shader_program)
+        glUniformMatrix4fv(
+            self.context.mvp_location, 1, GL_FALSE, glm.value_ptr(self.context.mvp)
+        )
 
-	def draw(self):
-		"""
-		The main drawing function. Is called whenever an update occurs.
-		"""
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-		self.calc_mvp()
-		glUseProgram(self.shader_program)
-		glUniformMatrix4fv(
-			self.context.mvp_location, 1, GL_FALSE, glm.value_ptr(self.context.mvp)
-		)
+        glEnableVertexAttribArray(0)
+        glBindBuffer(GL_ARRAY_BUFFER, self.context.vertex_buffer)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
 
-		glEnableVertexAttribArray(0)
-		glBindBuffer(GL_ARRAY_BUFFER, self.context.vertex_buffer)
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+        glEnableVertexAttribArray(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.context.color_buffer)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, None)
 
-		glEnableVertexAttribArray(1)
-		glBindBuffer(GL_ARRAY_BUFFER, self.context.color_buffer)
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, None)
+        glDrawArrays(GL_TRIANGLES, 0, len(vertex_buffer_data))
 
-		glDrawArrays(GL_TRIANGLES, 0, len(vertex_buffer_data))
-
-		glDisableVertexAttribArray(0)
-		glDisableVertexAttribArray(1)
-		glUseProgram(0)
+        glDisableVertexAttribArray(0)
+        glDisableVertexAttribArray(1)
+        glUseProgram(0)
 
 
 if __name__ == "__main__":
-	win = Win()
-	win.controller = MVPController(win.update_if, width=win.width, height=win.height)
-	win.init_opengl()
-	win.init_context()
-	win.run()
+    win = Win()
+    win.controller = MVPController(win.update_if, width=win.width, height=win.height)
+    win.init_opengl()
+    win.init_context()
+    win.run()
--- CG_assignment2/utils/mvp_controller.py
+++ CG_assignment2/utils/mvp_controller.py
@@ -7,7 +7,7 @@ class MVPController:
         self.callback_update = callback_update
         self.width = width
         self.height = height
-        self.position = glm.vec3(1, 1, -2)
+        self.position = glm.vec3(2, 2, 2.5)
         self.pitch = -0.5
         self.yaw = -0.5
         self.roll = 0.0
@@ -15,25 +15,16 @@ class MVPController:
         self.mouse_speed = 0.01
         self.fov = 90
         self.calc_view_projection()
+        self.last_x = 0
+        self.last_y = 0
 
     def calc_mvp(self, model_matrix=glm.mat4(1.0)):
         return self.projection_matrix * self.view_matrix * model_matrix
 
     def calc_view_projection(self):
-        # 3. Implement the direction, right and up vectors here.
-        # Currently none of them are correct
-        # self.direction = glm.vec3(-1, -1, 2)
-        # self.up = glm.vec3(0, -1, 0)
-        # self.right = glm.vec3(0, 1, 0)
-        self.direction = glm.vec3(
-            - math.cos(glm.radians(self.yaw)) * math.cos(glm.radians(self.pitch)),
-            # unsure about the - sign for X, but without it I didn't have anything displayed
-            math.sin(glm.radians(self.pitch)),
-            math.sin(glm.radians(self.yaw)) * math.cos(glm.radians(self.pitch))
-
-        )
-        self.right = glm.normalize(glm.cross(glm.vec3(0, 1, 0), self.direction))
-        self.up = glm.cross(self.direction, self.right)
+        self.direction = glm.vec3(-2, -2, -2.5)
+        self.up = glm.vec3(0, 1, 0)
+        self.right = glm.vec3(0, 1, 0)
         self.view_matrix = glm.lookAt(self.position,
                           self.position + self.direction,
                           self.up)
@@ -41,28 +32,12 @@ class MVPController:
         self.projection_matrix = glm.perspective(glm.radians(self.fov), self.width / self.height, 0.1, 1000)
 
     def on_keyboard(self, key: bytes, x: int, y: int):
-        # 4. Set the corresponding actions based on the key here
-        vector = glm.vec3(0, 0, 0)
-        if key == b'w':
-            # forward
-            vector = glm.vec3(+1, 0, 0)
-        if key == b'a':
-            # left
-            vector = glm.vec3(0, 0, +1)
-        if key == b'd':
-            # right
-            vector = glm.vec3(0, 0, -1)
-        if key == b's':
-            # backward
-            vector = glm.vec3(-1, 0, 0)
-        if key == b'e':
-            # up
-            vector = glm.vec3(0, +1, 0)
-        if key == b'r':
-            # down
-            vector = glm.vec3(0, -1, 0)
 
-        self.position -= vector * self.speed
+
+
+
+
+        
 
         self.calc_view_projection()
         self.callback_update()
@@ -79,8 +54,6 @@ class MVPController:
         self.last_y = y
         self.yaw -= y_diff * self.mouse_speed
         self.pitch -= x_diff * self.mouse_speed
-        print(self.yaw)
-        print(self.pitch)
         self.calc_view_projection()
         self.callback_update()
 
